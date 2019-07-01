b925f8063eb49c482d9f7f8aacc09978bbb082d8
lidemo <ggeorgiev15@gmail.com>
2019-07-01 08:34:05 +0100

    init

---
+++ PVXArtisan/Types/ReportType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace PVXArtisan\Types;
+
+use PVXArtisan\Contracts\PVXTemplateTypeContract;
+
+class ReportType implements PVXTemplateTypeContract{
+
+    public static function getInstance() 
+    {
+        return new static;
+    }
+
+    public function column(String $value) : String 
+    {
+        return "[$value]";
+    }
+
+    public function equals(String $value) : String 
+    {
+        return ".Equals(\"$value\")";
+    }
+
+    public function notEquals(String $value) : String 
+    {
+        
+    }
+
+    public function contains(String $value) : String 
+    {
+        return ".Contains(\"$value\")";
+    }
+
+    public function notContains(String $value) : String 
+    {
+
+    }
+
+    public function moreThan(String $value) : String
+    {
+        return "> \"$value\" ";
+    }
+
+    public function lessThan(String $value) : String
+    {
+        return "< \"$value\" ";
+    }
+
+    public function moreOrEquals(String $value) : String 
+    {
+        return ">= \"$value\" ";
+    }
+
+    public function lessOrEquals(String $value) : String
+    {
+        return "<= \"$value\" ";
+    }
+
+    public function startsWith(String $value) : String {
+        return ".StartsWith(\"$value\")";
+    }
+
+    public function endsWith(String $value) : String {
+        return ".EndsWith(\"$value\")";
+    }
+
+}
