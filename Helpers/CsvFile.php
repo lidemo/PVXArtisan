2b1f66ace47b298b23afd039b407da8d220f9ece
lidemo <ggeorgiev15@gmail.com>
2020-02-25 10:57:24 +0000

    fixed writeToFile folder creation

--- Helpers/CsvFile.php
+++ Helpers/CsvFile.php
@@ -7,6 +7,12 @@ class CsvFile
 
     public static function writeToFile($filePath, $csv)
     {
+
+        $exploded = explode('/', $filePath);
+        array_pop($exploded);
+        $directory = implode('/', $exploded);
+
+        if (!file_exists($directory)) mkdir($directory, 0777, true);
         file_put_contents($filePath, $csv);
     }
 
